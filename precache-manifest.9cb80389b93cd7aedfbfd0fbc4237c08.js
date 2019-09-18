5ad07a28cabf35c3bb8c4bb021e5ad5baf79e424
Joshua Ignacio <joshua.ignacio@dynameyes.com>
2019-09-18 09:44:57 +0800

    Updates

---
+++ precache-manifest.9cb80389b93cd7aedfbfd0fbc4237c08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf2731aca67dcb3a580bf38956501f33",
+    "url": "/sms-data-helper/index.html"
+  },
+  {
+    "revision": "6005a5c84de4dd613a61",
+    "url": "/sms-data-helper/static/css/main.34ddbca1.chunk.css"
+  },
+  {
+    "revision": "2d4ee1a9272c300f6c7e",
+    "url": "/sms-data-helper/static/js/2.8d96827a.chunk.js"
+  },
+  {
+    "revision": "6005a5c84de4dd613a61",
+    "url": "/sms-data-helper/static/js/main.f58275c8.chunk.js"
+  },
+  {
+    "revision": "b06e83f36c0287c21edf",
+    "url": "/sms-data-helper/static/js/runtime~main.b9003115.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/sms-data-helper/precache-manifest.9cb80389b93cd7aedfbfd0fbc4237c08.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sms-data-helper/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
